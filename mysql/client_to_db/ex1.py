c32d9d7f6c24078c79963b2d642e3f2cb1d19512
outboxltd <benatiya@gmail.com>
2019-07-23 14:34:15 +0300

    ex1 love it !!!

---
+++ mysql/client_to_db/ex1.py
@@ -0,0 +1,28 @@
+import pymysql
+
+actors = ['Andrew Adamson', 'William Addy', 'Seth Adkins']
+
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='19371937',
+                             db='imdb',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+try:
+    with connection.cursor() as cursor:
+        sql = "select * from actors"
+        cursor.execute(sql)
+
+        result = cursor.fetchall()
+        for i in result:
+            for g in actors:
+                if i['full_name'] == g:
+                    print(i['full_name'] + " is in the db")
+
+    connection.commit()
+
+finally:
+    connection.close()
+
