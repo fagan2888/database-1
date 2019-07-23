cc35bcfb1622b51476b375380b18df9776fd99ee
outboxltd <benatiya@gmail.com>
2019-07-23 14:36:50 +0300

    ex1

--- mysql/SQL Basics/star/ex3.sql
+++
@@ -1,8 +0,0 @@
--- SELECT dept_name FROM departments;
--- SELECT * FROM employees WHERE  gender = "F" and first_name = "Elvis";-- 
--- SELECT * FROM employees WHERE first_name in ("Georgi","Smith","Albin")
--- SELECT * FROM employees WHERE hire_date BETWEEN '1995-01-01' AND '1996-01-01';
- -- SELECT * FROM employees WHERE first_name LIKE 'A%' and last_name like '%z';
- -- select * from salaries order by salary desc limit 5;
- -- select * FROM employees WHERE hire_date = (SELECT hire_date FROM employees WHERE first_name = 'Jagoda');
- 
